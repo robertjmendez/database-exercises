4c0f664a99453cfa13a3f137cb827ca099a46b63
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-11 12:00:02 -0500

    Created the sql file

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED AUTO_INCREMENT,
+                        artist VARCHAR(255),
+                        name VARCHAR(255),
+                        release_date INT,
+                        sales FLOAT,
+                        genre VARCHAR(255),
+                        PRIMARY KEY (id)
+);
+
