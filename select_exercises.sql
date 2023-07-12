01093ba9636097699700a18feb89d7fa94f55eee
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-12 13:49:38 -0500

    Changed format

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,31 +1,19 @@
 USE codeup_test_db;
 
 -- The name of all albums by Pink Floyd.
-SELECT name
-FROM albums
-WHERE artist = 'Pink Floyd';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date
-FROM albums
-WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- The genre for Nevermind
-SELECT genre
-FROM albums
-WHERE name = 'Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
 -- Which albums were released in the 1990s
-SELECT name
-FROM albums
-WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales
-SELECT name
-FROM albums
-WHERE sales < 20;
+SELECT name FROM albums WHERE sales < 20;
 
 -- All the albums with a genre of "Rock".
-SELECT name
-FROM albums
-WHERE genre = 'Rock';
+SELECT name FROM albums WHERE genre = 'Rock';
