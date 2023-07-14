76d45c96e6350f2eccf860cf5350489cc360ba8b
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-14 13:28:12 -0500

    Created group_by_exercises.sql file

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,22 +1,14 @@
 USE employees;
 
--- Update your queries for employees whose names start and end with 'E'.
--- Use concat() to combine their first and last name together as a single column in your results.
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees WHERE first_name LIKE 'E%' AND last_name LIKE '%E';
 
--- Find all employees born on Christmas — 842 rows.
 SELECT * FROM employees WHERE DAY(birth_date) = 25 AND MONTH(birth_date) = 12;
 
--- Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND DAY(birth_date) = 25 AND MONTH(birth_date) = 12;
 
--- Change the query for employees hired in the 90s and born on Christmas such that
--- the first result is the oldest employee who was hired last. It should be Khun Bernini.
 SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND DAY(birth_date) = 25 AND MONTH(birth_date) = 12
 ORDER BY birth_date ASC, hire_date DESC;
 
--- For your query of employees born on Christmas and hired in the 90s,
--- use datediff() to find how many days they have been working at the company
 SELECT *, DATEDIFF(CURDATE(), hire_date) AS days_working
 FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND DAY(birth_date) = 25 AND MONTH(birth_date) = 12;
---
+++ group_by_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT COUNT(*), last_name FROM employees WHERE last_name IN ('Chleq', 'Lindqvist', 'Qiwen') GROUP BY last_name;
+
+SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+
+
+
+
+
