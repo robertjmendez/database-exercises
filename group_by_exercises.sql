8de222883db0134d4d2f68f0414a0d38bab4e479
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-15 08:12:33 -0500

    Added order by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,9 +2,9 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name;
+SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name ORDER BY last_name;
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY first_name, last_name;
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY first_name, last_name ORDER BY last_name;
 
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
