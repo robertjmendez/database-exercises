687bc45e0356058bb17c51dfc848fc93eeaba1cc
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-15 08:33:20 -0500

    Created join_exercises.sql file

---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+# Using the example in the Associative Table Joins section as a
+# guide, write a query that shows each department along with the
+# name of the current manager for that department.
+
+# Find the name of all departments currently managed by women.
+
+# Find the current titles of employees currently working in the Customer Service department.
+
+# Find the current salary of all current managers.
