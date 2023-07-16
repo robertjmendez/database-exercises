c528f61ab34ebce53c61d177179c41db394069a9
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-16 13:10:00 -0500

    Created join_exercises.sql file

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,9 +1,38 @@
 # Using the example in the Associative Table Joins section as a
 # guide, write a query that shows each department along with the
 # name of the current manager for that department.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no
+WHERE dm.to_date > CURDATE()
+ORDER BY d.dept_name;
 
 # Find the name of all departments currently managed by women.
 
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no
+WHERE dm.to_date > CURDATE() AND e.gender = 'F'
+ORDER BY d.dept_name;
+
 # Find the current titles of employees currently working in the Customer Service department.
 
+SELECT t.title, COUNT(*) AS 'Total'
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN titles t ON e.emp_no = t.emp_no
+WHERE d.dept_name = 'Customer Service' AND de.to_date > CURDATE() AND t.to_date > CURDATE()
+GROUP BY t.title;
+
 # Find the current salary of all current managers.
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no
+JOIN salaries s ON e.emp_no = s.emp_no
+WHERE dm.to_date > CURDATE() AND s.to_date > CURDATE()
+ORDER BY d.dept_name;
