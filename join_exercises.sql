653ed818ae1214343e29e2b001ab41b02691723c
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-17 11:16:00 -0500

    Updated join_exercises.sql, changed where clause

--- join_exercises.sql
+++ join_exercises.sql
@@ -5,7 +5,7 @@ SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) 
 FROM departments d
 JOIN dept_manager dm ON d.dept_no = dm.dept_no
 JOIN employees e ON dm.emp_no = e.emp_no
-WHERE dm.to_date > CURDATE()
+WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 # Find the name of all departments currently managed by women.
@@ -14,25 +14,25 @@ SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) 
 FROM departments d
 JOIN dept_manager dm ON d.dept_no = dm.dept_no
 JOIN employees e ON dm.emp_no = e.emp_no
-WHERE dm.to_date > CURDATE() AND e.gender = 'F'
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY d.dept_name;
 
 # Find the current titles of employees currently working in the Customer Service department.
 
-SELECT t.title, COUNT(*) AS 'Total'
-FROM employees e
-JOIN dept_emp de ON e.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no
-JOIN titles t ON e.emp_no = t.emp_no
-WHERE d.dept_name = 'Customer Service' AND de.to_date > CURDATE() AND t.to_date > CURDATE()
-GROUP BY t.title;
+SELECT title, COUNT(*) AS 'Total'
+FROM titles t
+JOIN dept_emp AS de ON t.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service' AND de.to_date = '9999-01-01' AND t.to_date = '9999-01-01'
+GROUP BY title;
 
 # Find the current salary of all current managers.
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
-FROM departments d
-JOIN dept_manager dm ON d.dept_no = dm.dept_no
-JOIN employees e ON dm.emp_no = e.emp_no
+FROM employees e
+JOIN dept_manager dm ON dm.emp_no = e.emp_no
+JOIN departments d ON dm.dept_no = d.dept_no
 JOIN salaries s ON e.emp_no = s.emp_no
-WHERE dm.to_date > CURDATE() AND s.to_date > CURDATE()
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
